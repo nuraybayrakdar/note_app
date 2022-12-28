bf0ef10c5c03859fe0ba604ff349876fd060071e
nuraybayrakdar <bayrakdarnuray@gmail.com>
2022-12-28 22:27:41 +0300

    note detail page added

--- lib/ui/pages/note_detail_page.dart
+++ lib/ui/pages/note_detail_page.dart
@@ -1,1 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:note_app/model/note.dart';
+import 'package:note_app/repository/notes_db.dart';
+import 'package:note_app/ui/pages/add_note_page.dart';
 
+class NoteDetailPage extends StatefulWidget {
+  final int noteId;
+
+  const NoteDetailPage({
+    Key? key,
+    required this.noteId,
+  }) : super(key: key);
+
+  @override
+  _NoteDetailPageState createState() => _NoteDetailPageState();
+}
+
+class _NoteDetailPageState extends State<NoteDetailPage> {
+  late Note note;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshNote();
+  }
+
+  Future refreshNote() async {
+    setState(() => isLoading = true);
+
+    this.note = await NotesDatabase.instance.readNote(widget.noteId);
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [editButton(), deleteButton()],
+        ),
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: const EdgeInsets.all(12),
+                child: ListView(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  children: [
+                    Text(
+                      note.title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      DateFormat.yMMMd().format(note.createdTime),
+                      style: const TextStyle(color: Colors.white38),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      note.description,
+                      style:
+                          const TextStyle(color: Colors.white70, fontSize: 18),
+                    )
+                  ],
+                ),
+              ),
+      );
+
+  Widget editButton() => IconButton(
+      icon: const Icon(Icons.edit_outlined),
+      onPressed: () async {
+        if (isLoading) return;
+
+        await Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => AddNotePage(note: note),
+        ));
+
+        refreshNote();
+      });
+
+  Widget deleteButton() => IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () async {
+          await NotesDatabase.instance.delete(widget.noteId);
+
+          Navigator.of(context).pop();
+        },
+      );
+}
